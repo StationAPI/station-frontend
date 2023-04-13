287d958fcbe1b7b6720c50758873b887e00266bd
FerretCode <674313@salemkeizer.org>
2023-04-13 18:32:07 +0000

    start frontend

---
+++ station-frontend/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="navbar text-primary-content" style="background-color: #8570A8;">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl" href="/">Home</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/dashboard">Dashboard</a></li>
+    </ul>
+  </div>
+</div>
+
+<slot />
+
+<style>
+  * {
+    font-family: "Courier New", Courier, monospace;
+  }
+</style>
---
+++ station-frontend/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="mb-5">Station</h1>
+
+<button class="btn">Hello daisyUI</button>
