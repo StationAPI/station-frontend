fd76d124e2cc0bbf313df3c81d8654b58b23e424
FerretCode <674313@salemkeizer.org>
2023-04-14 00:19:00 +0000

    continue frontend

--- station-frontend/src/routes/+page.svelte
+++ station-frontend/src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<h1 class="mb-5">Station</h1>
+<script>
+  import logo from "$lib/assets/hero.svg";
 
-<button class="btn">Hello daisyUI</button>
+  console.log(logo);
+</script>
+
+<div class="hero min-h-screen bg-base-200" style="background-color: #473E4E;">
+  <div class="hero-content flex-col lg:flex-row">
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img src={logo} alt="Hero Image" height="1000" width="1000" />
+    <div class="text-primary-content">
+      <h1 class="text-5xl font-bold">Grow Your Website</h1>
+      <h2 class="py-6 text-3xl">
+        Grow Your Audience with Station's Discoverability Tools
+      </h2>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<div class="flex justify-center">
+  <ul class="steps steps-vertical mb-5 text-primary-content">
+    <li class="step step-primary">Register</li>
+    <li class="step">Upload your Website</li>
+    <li class="step">Bump</li>
+    <li class="step">Grow your Traffic</li>
+  </ul>
+</div>
