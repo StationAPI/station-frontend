3ae5b19e983e1282e691249d23d2d4d20c670b58
FerretCode <674313@salemkeizer.org>
2023-04-17 20:42:51 +0000

    continue dashboard

--- station-frontend/src/routes/dashboard/+page.svelte
+++ station-frontend/src/routes/dashboard/+page.svelte
@@ -1,23 +1,35 @@
-<div class="grid grid-cols-4 gap-4">
-  <div class="drawer drawer-mobile">
-    <input id="drawer-2" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col">
-      <!-- Page content here -->
-      <label
-        for="drawer-2"
-        class="btn btn-primary drawer-button lg:hidden mt-1"
-        style="border-radius:0px;background-color:#8570A8;border-color:#8570A8"
-        >Open Menu</label
-      >
+<div class="grid grid-cols-3 gap-6 my-10 ml-10">
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <figure class="px-10 pt-10">
+      <img
+        src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes"
+        class="rounded-xl"
+      />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
     </div>
+  </div>
 
-    <div class="drawer-side">
-      <label for="drawer-2" class="drawer-overlay" />
-      <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-        <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-      </ul>
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <figure class="px-10 pt-10">
+      <img
+        src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes"
+        class="rounded-xl"
+      />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
     </div>
   </div>
 
