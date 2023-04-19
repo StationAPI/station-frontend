390cfc8326b7c41429d64a2d2f062cbaf52286fd
ferretcode <674313@salemkeizer.org>
2023-04-19 16:37:51 -0700

    responsiveness

--- station-frontend/src/routes/+page.svelte
+++ station-frontend/src/routes/+page.svelte
@@ -4,14 +4,14 @@
 </script>
 
 <div
-  class="hero min-h-min bg-base-200 mt-6"
+  class="hero min-h-min bg-base-200 mt-6" 
   style="background: rgb(87,73,99);
 background: linear-gradient(90deg, rgba(87,73,99,1) 0%, rgba(105,75,129,1) 32%, rgba(128,69,180,1) 63%, rgba(132,24,227,1) 100%);"
 >
-  <div class="hero-content flex-col lg:flex-row-reverse">
+  <div class="hero-content flex-col lg:scale-100 lg:flex-row-reverse sm:scale-75">
     <!-- svelte-ignore a11y-img-redundant-alt -->
-    <img src={hero2} alt="Hero Image" height="1000" width="1000" />
-    <div class="text-primary-content">
+	<img class="object-contain md:max-w-3xl rounded-lg" src={hero2} alt="Hero Image" />
+    <div class="text-primary-content text-center">
       <h1 class="text-5xl font-bold">Find Your New Favorite Platform</h1>
       <p class="py-6 text-3xl">
         Discover the Web's Best with Station's Detailed Filters.
@@ -32,8 +32,8 @@ background: linear-gradient(90deg, rgba(87,73,99,1) 0%, rgba(105,75,129,1) 32%, 
 >
   <div class="hero-content flex-col lg:flex-row">
     <!-- svelte-ignore a11y-img-redundant-alt -->
-    <img src={hero1} alt="Hero Image" height="1000" width="1000" />
-    <div class="text-primary-content">
+    <img class="object-contain md:max-w-3xl sm:px-5" src={hero1} alt="Hero Image" />
+    <div class="text-primary-content text-center px-5">
       <h1 class="text-5xl font-bold">Grow Your Website</h1>
       <h2 class="py-6 text-3xl">
         Grow Your Audience with Station's Discoverability Tools
